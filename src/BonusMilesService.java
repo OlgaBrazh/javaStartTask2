239598cbabad5d34b8694f487b4f51c5591af593
Olga Brazhkina <emelyanova84@yandex.ru>
2022-11-30 23:02:03 +0500

    Task 3 done

---
+++ src/BonusMilesService.java
@@ -0,0 +1,6 @@
+public class BonusMilesService {
+    public int calculate (int cost, int unitmile) {
+    int bonusMile = (cost/unitmile);
+    return bonusMile;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
-        int cost = 80;
-        int unitMile = 20;
-        int bonusMile = ( cost / unitMile);
-
-        System.out.println("Количество бонусных миль: " + bonusMile);
+        BonusMilesService service = new BonusMilesService();
+        int miles = service.calculate(100000,20);
+        System.out.println("Количество бонусных миль: " + miles);
     }
 }
